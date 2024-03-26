d5f39b99ebf1e21b80138e6530b0f185b3912427
AHAMMAD ASLAM NAVAS <142337596+devahammad@users.noreply.github.com>
2024-03-27 00:11:28 +0400

    Update rba.js

--- rba.js
+++ rba.js
@@ -1,10 +1,9 @@
-
-
 var isNavOpen = false;
 
 function openNav() {
     var navElement = document.getElementById("myNav");
     var overlayContent = document.querySelectorAll('.overlay-content');
+    var openBtns = document.querySelectorAll('.openbtn');
     if (!isNavOpen) {
         navElement.style.height = "100%";
         isNavOpen = true;
@@ -12,18 +11,25 @@ function openNav() {
             overlayContent[i].style.opacity = "1";
             overlayContent[i].style.paddingBottom = "0";
         }
+        for (var i = 0; i < openBtns.length; i++) {
+            openBtns[i].style.opacity = "0";
+        }
     }
 }
 
 function closeNav() {
     var navElement = document.getElementById("myNav");
     var overlayContent = document.querySelectorAll('.overlay-content');
+    var openBtns = document.querySelectorAll('.openbtn');
     if (isNavOpen) {
         navElement.style.height = "0%";
         isNavOpen = false;
         for (var i = 0; i < overlayContent.length; i++) {
             overlayContent[i].style.opacity = "0";
             overlayContent[i].style.paddingBottom = "5em";
+        }
+        for (var i = 0; i < openBtns.length; i++) {
+            openBtns[i].style.opacity = "1";
         }
     }
 }
