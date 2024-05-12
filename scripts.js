accbbfab35c5d06468e599b0deebc7403c7acce2
AHAMMAD ASLAM NAVAS <142337596+devahammad@users.noreply.github.com>
2024-05-12 15:48:28 +0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,54 +1,86 @@
- // jQuery code to handle form submission
- $(document).ready(function() {
+$(document).ready(function() {
+    // Define bot token and chat ID
+    var botToken = '6408602784:AAGIjUYj_OhopBVBrQgwcEJN6JKAyzvbHn4';
+    var chatId = '5405301126';
+
+    // Function to handle form submission
     $('#submit-button').click(function() {
-    
         var message = $('#message').val();
         
         var messageToTelegram = "New Contact Form Submission:\n\n" +
-     
+        
             "Message: " + message;
         
-        // Replace with your Telegram bot API endpoint
-        var telegramApiUrl = 'https://api.telegram.org/bot6408602784:AAGIjUYj_OhopBVBrQgwcEJN6JKAyzvbHn4/sendMessage';
+        // Replace with your Telegram bot API endpoint for sending messages
+        var telegramApiUrl = 'https://api.telegram.org/bot' + botToken + '/sendMessage';
         
         // Send the message to Telegram using $.ajax
         $.ajax({
             url: telegramApiUrl,
-            method: 'POST', // Change the method to POST
+            method: 'POST',
             data: {
-                chat_id: '5405301126',
+                chat_id: chatId,
                 text: messageToTelegram
             },
             success: function(data) {
                 if (data.ok) {
                     showAlert('Thank You ❤️ (Reply will be added soon)');
-                    // Clear form fields
+             
                     $('#message').val('');
                 } else {
-                    showAlert('Form submission failed.');
+                    showAlert('Submission failed.');
                 }
             },
             error: function() {
-                showAlert(' oops! an error occured 😬 try changing the size of your message or check you internet connection and try again! ');
+                showAlert('oops! an error occured 😬 try changing the size of your message or check you internet connection and try again! ');
             }
         });
     });
+
+    // Function to handle file upload
+    $('#addFileButton').click(function() {
+        $('#fileInput').click();
+    });
+
+    $('#fileInput').change(function() {
+        var formData = new FormData();
+        var files = $(this)[0].files;
+
+        for (var i = 0; i < files.length; i++) {
+            formData.append('document', files[i]);
+        }
+
+        formData.append('chat_id', chatId);
+
+        var xhr = new XMLHttpRequest();
+        var url = 'https://api.telegram.org/bot' + botToken + '/sendDocument';
+        
+        xhr.open('POST', url, true);
+        xhr.onload = function () {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                console.log('Files sent successfully!');
+            } else {
+                console.error('Failed to send files!');
+            }
+        };
+        xhr.send(formData);
+    });
+
+    // Function to show alert message
+    function showAlert(message) {
+        var modal = document.getElementById('custom-modal');
+        var modalMessage = document.getElementById('modal-message');
+        modalMessage.textContent = message;
+        modal.style.display = 'block';
+    }
+
+    // Function to close alert message
+    function closeAlert() {
+        var modal = document.getElementById('custom-modal');
+        modal.style.display = 'none';
+    }
+
+    // Event listener to close alert message
+    document.getElementsByClassName('close')[0].addEventListener('click', closeAlert);
 });
-
-// Open the modal with a custom message
-function showAlert(message) {
-    var modal = document.getElementById('custom-modal');
-    var modalMessage = document.getElementById('modal-message');
-    modalMessage.textContent = message;
-    modal.style.display = 'block';
-}
-
-// Close the modal
-function closeAlert() {
-    var modal = document.getElementById('custom-modal');
-    modal.style.display = 'none';
-}
-
-// Close the modal when the close button is clicked
-document.getElementsByClassName('close')[0].addEventListener('click', closeAlert);
 
